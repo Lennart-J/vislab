ed5d06979be8b7db0acedc73c9ecab989fa9f8b9
Lennart-J <l.jarms@hotmail.de>
2014-12-01 19:21:22 +0100

    webshop page

--- src/vislab/model/bl/ProductManager.java
+++ src/vislab/model/bl/ProductManager.java
@@ -1,7 +1,12 @@
 package vislab.model.bl;
 
-import org.hibernate.Session;
+import java.util.List;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
+import vislab.model.db.Category;
 import vislab.model.db.Product;
 import vislab.model.sf.HibernateUtil;
 
@@ -37,5 +42,50 @@ public class ProductManager {
 		session.beginTransaction();
 		session.save(product);
 		session.getTransaction().commit();
+	}
+
+	public List<Product> searchProduct(String name, String description,
+			String category, String minPrice, String maxPrice) {
+		Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+
+		session.beginTransaction();
+		Criteria criteria = session.createCriteria(Product.class);
+		addLikeRestriction(criteria, "name", name);
+		addLikeRestriction(criteria, "description", "%"+description+"%");
+		addLikeRestriction(criteria, "Category.name", category);
+		addGtRestriction(criteria, "price", minPrice);
+		addLtRestriction(criteria, "price", maxPrice);
+
+		List<Product> products = criteria.list();
+		session.getTransaction().commit();
+		return products;
+	}
+
+	private void addLikeRestriction(Criteria criteria,
+			String propertyName, Object value) {
+		if (value != null && !value.equals("")) {
+			criteria.add(Restrictions.like(propertyName, value));
+		}
+	}
+	
+	private void addEqRestriction(Criteria criteria,
+			String propertyName, Object value) {
+		if (value != null && !value.equals("")) {
+			criteria.add(Restrictions.like(propertyName, value));
+		}
+	}
+
+	private void addGtRestriction(Criteria criteria,
+			String propertyName, Object value) {
+		if (value != null && !value.equals("")) {
+			criteria.add(Restrictions.gt(propertyName, Double.parseDouble(value.toString())));
+		}
+	}
+
+	private void addLtRestriction(Criteria criteria,
+			String propertyName, Object value) {
+		if (value != null && !value.equals("")) {
+			criteria.add(Restrictions.lt(propertyName, Double.parseDouble(value.toString())));
+		}
 	}
 }
